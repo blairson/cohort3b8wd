f082591131d3b19878b42ee8b1b6d2e5461261ce
lami0987 <bukoyeodunlami2@gmail.com>
2021-02-03 08:06:30 -0500

    Lami first comit

---
+++ chort-3b8 week/files_and_builtins.py
@@ -0,0 +1,65 @@
+# # filename ="mydata.txt"
+# # excel ="wkbk.csv"
+
+# # # file = open("mydata.txt", "r")
+
+# # # print(file.read())
+
+# # # file.close()
+
+# # # file = open("mydata.txt", "a+")
+
+# # # file.write("\n\nThis is a new info which i forgot earlier.")
+# # # file.close()
+
+# # # file = open("mydata.txt", "w")
+# # # file.write("OOOPS this file has now been cleared.")
+
+# # # with open(filename, "a") as file:
+# # #     file.write("bla bla bla")
+
+# # # with open(filename, "r") as file:
+# # #     print(file.read())
+
+# # with open(excel, "w") as file:
+# #     print(file.write("ade, kunle, sunbo\n1,2,3"))
+
+# # with open("wkbk.csv", "r") as file:
+# #     print(file.read())
+
+# # import math
+# # a = [3,8,4,7,5,8,7]
+# # b = [a,b,c,d,e,f,g]
+# # c = dict
+# # print(sum(a))
+
+
+# name = input("please enter your name: ")
+# age = input("please enter your age: ")
+# hobby1 = input("please enter your first hobby: ")
+# hobby2 = input("please enter your second hobby: ")
+
+# print(name)
+# print(age)
+# print(hobby1)
+# print(hobby2)
+
+# print("\n", name.title(), "is", age, "years old. \nhobbies are listed below.")
+# print("\t-", hobby1)
+# print("\t-", hobby2)
+
+
+# drama = '''Ali is a good boy, he sings afro beat and likes to experiment. 
+# One day, Ali wentto the board of directors,
+
+# This is what he said,
+
+# "I think im ready to take over the company".
+#                     REgards,
+#                     Alaba,
+#                     Story for the gods.
+# '''
+
+# print(drama)
+
+print(divmod(502,45 ))

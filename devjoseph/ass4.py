fd4f03925a0ee1a0c9ca2dc1a8e1a576ebf2e189
Joseph Chinedu <josephchinedu34@yahoo.com>
2021-02-05 13:56:17 +0100

    update

--- devjoseph/ass4.py
+++ devjoseph/ass4.py
@@ -4,5 +4,5 @@
 firstname = input("enter your first name: ")
 lastname = input("enter your last name: ")
 
-file = open(f"./devjoseph/{lastname}.txt", "w")
+file = open(f"./devjoseph/week 3 assnm/{lastname}.txt", "w")
 file.write(firstname)

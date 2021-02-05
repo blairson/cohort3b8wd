fab8f70761f434893f6f46642bcea7178d962433
Joseph Chinedu <josephchinedu34@yahoo.com>
2021-02-05 13:49:10 +0100

    added assignment folder

---
+++ devjoseph/ass4.py
@@ -0,0 +1,8 @@
+# write a program that takes a name and last name seperated by comma and write the first name into
+# a file named the last name
+
+firstname = input("enter your first name: ")
+lastname = input("enter your last name: ")
+
+file = open(f"{lastname}.txt", "w")
+file.write(firstname)

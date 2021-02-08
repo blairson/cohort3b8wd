c02488eee117307b92502d095953ac1d5be5485a
Your Name <you@example.com>
2021-02-08 11:19:58 +0100

    assignment done

--- michealfolder.py
+++ michealfolder.py
@@ -1,6 +1,6 @@
 # ASSIGNMENT
 
-
+# # assigment 1
 # A = int (input ("enter the value of A: "))
 # VALUE_A = A , A * A, A ** A
 # print (VALUE_A)
@@ -8,19 +8,19 @@
 # # A = int (input ("enter the value of A: "))
 # print ( A ,+ A * A , + A ** A)
 # # Palindrome
-
+# # assignment 2
 # word= input ("write a word: ")
 # if(word == word [::-1]):
 #     print ("the word is palindrome")
 # else:
 #     print ("not a palindrome")
-
+# # assignment 3
 # # print string
 # def my_function(xyz):
 #      return xyz[::-1]
-# xyz= my_function("HELLO MY UNIVELCITY PEOPLE")
-# print (xyz)
-
+# # xyz= my_function("HELLO MY UNIVELCITY PEOPLE")
+# # print (xyz)
+# # # assignment 4
 # firstname = input("my first name : ")
 # lastname = input("my last name : ")
 # file = open(f"{lastname}.txt", "w")
